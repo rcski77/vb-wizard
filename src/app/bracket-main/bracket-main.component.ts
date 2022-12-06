820a4de83fd175595145d65d2a01763646227d3b
rcski77 <rcski77@gmail.com>
2022-12-05 20:47:22 -0500

    started to stub out bracket component

--- src/app/bracket-main/bracket-main.component.ts
+++ src/app/bracket-main/bracket-main.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Team } from '../team';
 
 @Component({
   selector: 'app-bracket-main',
   templateUrl: './bracket-main.component.html',
   styleUrls: ['./bracket-main.component.css']
 })
-export class BracketMainComponent {
+export class BracketMainComponent implements OnInit{
+
+  bracket: any[] = []; //create array to generate bracket
+
+  generateBracket(size: number): void {
+    if (size > 2 && size <= 4) {
+      for (let row = 0; row < 3; row++) {
+        for (let column = 0; column < 2; column++) {
+          this.bracket.push({row: row, column: column});
+        }
+      }
+    }
+  } //end generate bracket
+
+  ngOnInit(): void {
+    this.generateBracket(4);
+  }
 
 }
